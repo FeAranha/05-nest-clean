300c6b28cd4e4a9d49720685978f0892b21aa37c
FeAranha <felipearanha.c@gmail.com>
2024-01-15 12:08:17 -0300

    casos de uso: autenticacao e cadastro

--- src/domain/forum/application/cryptography/hash-comparer.ts
+++ src/domain/forum/application/cryptography/hash-comparer.ts
@@ -1,3 +1,3 @@
-export abstract class HashGenerator {
-  abstract hash(plain: string): Promise<string>
+export abstract class HashComparer {
+  abstract compare(plain: string, hash: string): Promise<boolean>
 }
